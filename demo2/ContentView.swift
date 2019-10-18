5fdc963aa00819e4532546d48d410212d6d5103d
tanghaoshuai <1648375651@qq.com>
2019-10-18 08:53:49 +0800

    Initial Commit

---
+++ demo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo2
+//
+//  Created by 2017yd2 on 2019/10/18.
+//  Copyright © 2019 2017yd2. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
