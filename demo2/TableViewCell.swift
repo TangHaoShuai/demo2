3b1a25784cfd8b15815519d349353f1aa1fb7f14
tanghaoshuai <1648375651@qq.com>
2019-10-25 10:19:50 +0800

    111

---
+++ demo2/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  demo2
+//
+//  Created by 2017yd2 on 2019/10/25.
+//  Copyright © 2019 2017yd2. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
